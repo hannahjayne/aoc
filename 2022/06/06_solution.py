8c458e2acdc11d520742edfe639285bf7c311cdd
hannah-fullard <hfullard2000@gmail.com>
2022-12-06 11:49:56 +0000

    Uploading Day 6

---
+++ 2022/06/06_solution.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+FILE = "input"
+PACKET_MARKER = 4
+MESSAGE_MARKER =14
+
+def do_tasks(marker_type, file_name = FILE):
+    file = open(file_name, "r")
+    for line in file: signal = line[:-1]
+    file.close()
+    
+    for i in range (0, len(signal)): 
+        if len(set(signal[i:i+marker_type])) == marker_type: return i+marker_type
+ 
+def print_solutions():
+    print("TASK 1:",do_tasks(PACKET_MARKER))
+    print("TASK 2:",do_tasks(MESSAGE_MARKER))
+
+print_solutions()
